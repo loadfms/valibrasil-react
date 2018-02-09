44965a09a1122e06b6628ad4108f498122681927
lsilva <lsilva@webmotors.com.br>
2018-02-09 18:04:33 -0200

    add - cards link

--- app/components/Product/Card.js
+++ app/components/Product/Card.js
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom';
 
 const Card = () => {
   return (
-    <li className="products--list__card">
-      <img alt="product" src="https://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/m/ac/macbook/select/macbook-select-space-gray-201706?wid=452&hei=420&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1505775431709" />
-      <span className="products--list__card__title">Macbook Air</span>
+    <Link to='/product/1'>
+      <li className="products--list__card">
+        <img alt="product" src="https://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/m/ac/macbook/select/macbook-select-space-gray-201706?wid=452&hei=420&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1505775431709" />
+        <span className="products--list__card__title">Macbook Air</span>
         <Link to='/product/1' className="general--button">Home</Link>
-    </li>
+      </li>
+    </Link>
   );
 };
 
