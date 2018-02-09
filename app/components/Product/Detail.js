da99862c7314a4e28e71a460c396d56e9673befb
lsilva <lsilva@webmotors.com.br>
2018-02-09 19:29:21 -0200

    add - product detail

--- app/components/Product/Detail.js
+++ app/components/Product/Detail.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Card = (props) => {
-  return (
-    <div className="product">
-      <div className="product--image">
-        <img src="https://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/m/ac/macbook/select/macbook-select-space-gray-201706?wid=452&hei=420&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1505775431709" />
-      </div>
-      <div className="product--detail">
-        <span className="product--detail__title">Macbook Air</span>
-        <span className="product--detail__subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fermentum sed felis ut pretium. Cras hendrerit a elit in vehicula. Vivamus non feugiat nisi. Vivamus at ultricies nisi.</span>
-      </div>
-    </div>
-  );
-};
+class Detail extends Component {
+  constructor(props) {
+    super(props);
 
-export default Card;
+    this.state = {
+      item: undefined
+    }
+  }
+
+  componentWillMount() {
+    this.loaditem();
+  }
+
+  loaditem() {
+    let _this = this;
+    //axios.get('https://apimagiccatalog.herokuapp.com/api/valibrasil/product/' + this.props.match.params.id)
+    axios.get('https://apimagiccatalog.herokuapp.com/api/valibrasil/product/cover')
+      .then(function (response) {
+        console.log(response.data.rows[0]); // ex.: { user: 'Your User'}
+        _this.setState({ item: response.data.rows[0] });
+      });
+  }
+
+  render() {
+    if (this.state.item) {
+      return (
+        <div className="product">
+          <div className="product--image">
+            <img src={this.state.item.image} />
+          </div>
+          <div className="product--detail">
+            <span className="product--detail__title">{this.state.item.name}</span>
+            <span className="product--detail__subtitle">{this.state.item.description}</span>
+          </div>
+        </div>
+      );
+    }
+    return '';
+  }
+}
+
+export default Detail;
