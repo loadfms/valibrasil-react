0526eee47bbae87271ac9e373a3f1f1019546edc
loadfms@gmail.com <loadfms@gmail.com>
2018-06-15 21:49:38 -0300

    add - title in img

--- app/components/Product/Detail.js
+++ app/components/Product/Detail.js
@@ -29,7 +29,7 @@ class Detail extends Component {
       return (
         <div className="product">
           <div className="product--image">
-            <img src={this.state.item.image} />
+            <img src={this.state.item.image} alt={props.product.name.toLowerCase()} title={props.product.name.toLowerCase()} />
           </div>
           <div className="product--detail">
             <span className="product--detail__title">{this.state.item.name}</span>
